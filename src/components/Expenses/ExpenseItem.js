3e68367488be33d842b136b558b7191251c79c42
tucelek@gmail.com <tucelek@gmail.com>
2022-04-06 14:56:48 +0200

    deploy fixes

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,3 @@
-import react from 'react'
 import Card from '../UI/Card'
 import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css'
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,4 +1,4 @@
-import react, { useState } from 'react'
+import { useState } from 'react'
 import Card from '../UI/Card'
 import './Expenses.css'
 import ExpensesFilter from './ExpensesFilter'
